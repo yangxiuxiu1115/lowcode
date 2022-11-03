2b20d0fd3e4b5275dd3f43eae0f3044b99d815e7
‘xiuxiu-yang’ <1974364190@qq.com>
2022-11-03 14:48:31 +0800

    fix: 文件结构修改

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,20 +1,20 @@
 import { App } from '@lowcode/concept'
 import Page from './pages/Layout/Page'
-import { useState } from 'react'
-import Transition from './components/Animation/Transition'
+import Toolbar from './pages/Toolbar/Toolbar'
 
+import { Layout } from 'antd'
+
+const { Header } = Layout
 function Root() {
   const app = new App({})
-  const [show, setShow] = useState(false)
 
   return (
-    <>
-      <Transition state={show} start="backInUp" end="backOutDown">
-        <div>123123</div>
-      </Transition>
-      <button onClick={() => setShow(!show)}>切换</button>
+    <Layout>
+      <Header>
+        <Toolbar />
+      </Header>
       <Page />
-    </>
+    </Layout>
   )
 }
 
