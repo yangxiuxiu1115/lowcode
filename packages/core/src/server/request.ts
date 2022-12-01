80e17cc64c1ecfec64a82d180b0bc342abc883f8
‘xiuxiu-yang’ <1974364190@qq.com>
2022-12-01 17:24:48 +0800

    feat: axios增加retry和Limit请求函数

--- packages/core/src/server/request.ts
+++ packages/core/src/server/request.ts
@@ -64,6 +64,57 @@ class HTTP {
       ...config,
     })
   }
+
+  retry<T>(config: RequestConfig, limit: number = 3) {
+    return new Promise((resolve, reject) => {
+      this.request<T>(config)
+        .then((value) => {
+          resolve(value)
+        })
+        .catch((err) => {
+          if (limit) {
+            this.retry<T>(config, limit--)
+              .then((val) => {
+                resolve(val)
+              })
+              .catch((err) => {
+                reject(err)
+              })
+          } else {
+            reject(err)
+          }
+        })
+    })
+  }
+
+  Limit<T>(configList: RequestConfig[], max: number = 5) {
+    return new Promise((resolve, reject) => {
+      const len = Math.min(configList.length, max)
+      const values: T[] = []
+      let index: number = 0
+      const start = () => {
+        if (index === configList.length) {
+          resolve(values)
+          return
+        }
+        const config = configList[index]
+        this.request<T>(config)
+          .then((val) => {
+            values[index++] = val
+            start()
+          })
+          .catch((err) => {
+            values[index++] = err
+            start()
+          })
+      }
+
+      while (index < len) {
+        start()
+        index++
+      }
+    })
+  }
 }
 
 export default HTTP
